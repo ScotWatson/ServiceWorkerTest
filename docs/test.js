dcb76e5bd045574916d2c8dbc38ea79db5ee609d
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2023-12-23 07:45:52 -0500

    Create test.js

---
+++ docs/test.js
@@ -0,0 +1,49 @@
+/*
+(c) 2023 Scot Watson  All Rights Reserved
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+const initPageTime = performance.now();
+
+const asyncWindow = new Promise(function (resolve, reject) {
+  window.addEventListener("load", function (evt) {
+    resolve(evt);
+  });
+});
+
+(async function () {
+  try {
+    const modules = await Promise.all( [ asyncWindow ] );
+    start(modules);
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+})();
+
+async function start( [ evtWindow ] ) {
+  try {
+    console.log(evtWindow);
+    navigator.serviceWorker.register();
+  } catch (e) {
+    console.log(e);
+  }
+  async function getRegistration() {
+    if ("serviceWorker" in navigator) {
+      try {
+        const registration = await navigator.serviceWorker.register("/sw.js", {
+          scope: "/ServiceWorkerTest/",
+        });
+        if (registration.installing) {
+          console.log("Service worker installing");
+        } else if (registration.waiting) {
+          console.log("Service worker installed");
+        } else if (registration.active) {
+          console.log("Service worker active");
+        }
+      } catch (error) {
+        console.error(`Registration failed with ${error}`);
+      }
+    }
+  }
+}
